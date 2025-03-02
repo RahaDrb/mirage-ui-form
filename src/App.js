b1c0ddf10de0ee5327fbb6f247d915761d1f8d69
raha <dararah06@gmail.com>
2025-03-02 21:54:11 +0330

    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import MainFormWrapper from "./components/form/MainFormWrapper";
+
+function App() {
+  return (
+    <div className="App">
+      <MainFormWrapper/>
+    </div>
+  );
+}
+
+export default App;
